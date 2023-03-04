67d8fd918c37a4bef7ccc39f12396f80c41bb746
SUDHARANIJANGA <122001053+SUDHARANIJANGA@users.noreply.github.com>
2023-03-04 21:30:18 +0530

    Add files via upload

---
+++ matching character.py
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    regex_pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
+    return 0;
+}
---
+++ matching specific 1.py
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+ 
+Regex_Pattern = r'^[123][012][xs0][30Aa][xsu][\.,]$'    
+    return 0;
+}
